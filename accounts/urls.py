59d3c2eea87b87294b2e43807959f9701878e02d
JongwonYoun <jeffyoun@naver.com>
2020-08-08 02:08:34 +0900

    FEAT: 로그아웃 기능 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from main import views
 from . import views
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('signup', views.signup, name="signup"),
     path('login', LoginView.as_view() , name="login"),
+    path('logout', LogoutView.as_view() , name="logout"),
 ]
 
 
