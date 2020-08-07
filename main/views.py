2867712486f3ce7df8bf9d413a902c9121e7df09
JongwonYoun <jeffyoun@naver.com>
2020-08-08 01:43:31 +0900

    FEAT: 수정 기능 추가

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import JssForm
 from .models import Jasoseol
+from django.http import Http404
 
 # Create your views here.
 def index(request):
@@ -21,16 +22,28 @@ def create(request):
         return render(request, 'create.html', {'jss_form' : jss_form})
 
 def detail(request, jss_id):
-    
-    jss = Jasoseol.objects.get(pk=jss_id)
+
+    jss = get_object_or_404(Jasoseol, pk=jss_id)
 
 
     return render(request, 'detail.html', {'jss' : jss })
 
 def delete(request, jss_id):
     
-    jss = Jasoseol.objects.get(pk=jss_id)
+    jss = get_object_or_404(Jasoseol, pk=jss_id)
 
     jss.delete()
 
     return redirect('index')
+
+def update(request, jss_id):
+    jss = get_object_or_404(Jasoseol, pk=jss_id)
+    jss_form = JssForm(instance=jss)
+
+    if request.method == "POST":
+        updated_form = JssForm(request.POST, instance=jss)
+        if updated_form.is_valid():
+            updated_form.save()
+            return redirect('detail', jss_id)
+
+    return render(request, 'create.html', {'jss_form' : jss_form})
